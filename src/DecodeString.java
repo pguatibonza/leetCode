84ae3ae23eddc164cd4a646fb90f820b30643c18
Pablo Guatibonza <p.guatibonza@uniandes.edu.co>
2022-09-22 13:36:32 -0500

    clases

---
+++ src/DecodeString.java
@@ -0,0 +1,9 @@
+public class DecodeString {
+    public String decodeString(String s) {
+        
+    }
+    public static void main(String[] args) {
+        DecodeString d=new DecodeString();
+        System.out.println(d.decodeString("3[a]2[bc]"));
+    }
+}
